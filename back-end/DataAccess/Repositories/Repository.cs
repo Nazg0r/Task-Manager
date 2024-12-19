2d79eace8596f97e2586c08bd8d6cb273611a129
Nazg0r <gorchinshiy83@gmail.com>
2024-12-19 17:51:51 +0200

    feat: implement main Repository class

---
+++ back-end/DataAccess/Repositories/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Data.Entities;
+using Data.Interfaces;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+	public class Repository<TEntity>(DataContext DbContext) : IRepository<TEntity> where TEntity : BaseEntity
+	{
+		public DbSet<TEntity> entities => DbContext.Set<TEntity>();
+
+		public async Task AddAsync(TEntity entity) =>
+			await entities.AddAsync(entity);
+
+		public void Delete(TEntity entity) =>
+			 entities.Remove(entity);
+
+		public async Task DeleteByIdAsync(int id)
+		{
+			TEntity? entity = await entities.FindAsync(id);
+			if (entity is not null)
+				entities.Remove(entity);
+		}
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync() =>
+			await entities.ToListAsync();
+
+		public async Task<TEntity?> GetByIdAsync(int id) =>
+			await entities.FindAsync(id);
+
+		public void Update(TEntity entity) =>
+			entities.Update(entity);
+	}
+}
