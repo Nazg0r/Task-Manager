a8f3baf542b5f307328a488501ea95de87074ac9
Nazg0r <gorchinshiy83@gmail.com>
2024-12-19 15:48:12 +0200

    feat: create db context

---
+++ back-end/DataAccess/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Data
+{
+	public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+	{
+		public DbSet<User> Users { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<Entities.Task> Tasks { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Entities.Task>()
+					.HasMany(t => t.Employees)
+					.WithMany(e => e.Tasks)
+					.UsingEntity(j => j.ToTable("tasks_employees"));
+		}
+	}
+}
