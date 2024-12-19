1d4b6c1281f402f0837999dad4dc1c58b101c726
Nazg0r <gorchinshiy83@gmail.com>
2024-12-19 15:54:19 +0200

    feat: connect dbContect to project

--- back-end/Server/Program.cs
+++ back-end/Server/Program.cs
@@ -1,8 +1,14 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+	opt.UseNpgsql(builder.Configuration.GetConnectionString("DbConnection"));
+});
 var app = builder.Build();
 
 app.UseHttpsRedirection();
