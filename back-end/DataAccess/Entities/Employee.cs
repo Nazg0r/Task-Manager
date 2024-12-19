d1592d9305ef9069614a44cd76691dd775b143b1
Nazg0r <gorchinshiy83@gmail.com>
2024-12-19 13:03:12 +0200

    feat: add basic entities

---
+++ back-end/DataAccess/Entities/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess.Entities
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public string Workload { get; set; }
+		public IEnumerable<Task> Tasks { get; set; }
+	}
+}
---
+++ back-end/DataAccess/Entities/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace DataAccess.Entities
+{
+	public class Task
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Priority { get; set; }
+		public string State { get; set; }
+		public DateTime TimeToFinish { get; set; }
+		public DateTime CreationDate { get; set; }
+		public IEnumerable<Employee> Employees { get; set; }
+	}
+}
