b68575fb4d312e00542931baf7aeb40313cb0540
Nazg0r <gorchinshiy83@gmail.com>
2024-12-20 13:56:57 +0200

    implement EmployeeRepository

---
+++ back-end/DataAccess/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Interfaces;
+using DataAccess.Entities;
+
+namespace DataAccess.Interfaces
+{
+	public interface IEmployeeRepository : IRepository<Employee>
+	{
+		Task<Employee?> GetByFullNameAsync(string name, string surname);
+	}
+}
---
+++ back-end/DataAccess/Repositories/EmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataAccess.Data;
+using DataAccess.Entities;
+using DataAccess.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Repositories
+{
+	public class EmployeeRepository(DataContext dbContext) : Repository<Employee>(dbContext), IEmployeeRepository
+	{
+		public async Task<Employee?> GetByFullNameAsync(string name, string surname) =>
+			await entities.FirstOrDefaultAsync(e => e.User.Name == name && e.User.Surname == surname);
+	}
+}
