7590693acb779ac3a4f4d06bd297976adadb8363
Nazg0r <gorchinshiy83@gmail.com>
2024-12-20 14:04:48 +0200

    feat: implement UnitOfWork class

---
+++ back-end/DataAccess/Data/UnitOfWork.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Interfaces;
+using DataAccess.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+	public class UnitOfWork(DataContext dbContext) : IUnitOfWork
+	{
+		public IUserRepository UserRepository => new UserRepository(dbContext);
+
+		public IEmployeeRepository EmployeeRepository => new EmployeeRepository(dbContext);
+
+		public ITaskRepository TaskRepository => new TaskRepository(dbContext);
+
+		public Task SaveAsync() => 
+			dbContext.SaveChangesAsync();
+	}
+}
---
+++ back-end/DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Interfaces
+{
+	public interface IUnitOfWork
+	{
+		IUserRepository UserRepository { get; }
+		IEmployeeRepository EmployeeRepository { get; }
+		ITaskRepository TaskRepository { get; }
+		Task SaveAsync();
+	}
+}
