87a415129a5396ec68570844d2ad6d10dfac07d3
Nazg0r <gorchinshiy83@gmail.com>
2024-12-20 18:22:24 +0200

    feat: add EmployeeSeeder

---
+++ back-end/Tests/Seeds/EmployeeSeeder.cs
@@ -0,0 +1,22 @@
+﻿using DataAccess.Entities;
+
+namespace Tests.Seeds
+{
+	public static class EmployeeSeeder
+	{
+		public static IEnumerable<Employee> PreparedEmployees =>
+		new Employee[]
+		{
+				new() { UserId = 1, Workload = "free"},
+				new() { UserId = 2, Workload = "free"},
+				new() { UserId = 3, Workload = "has few tasks"},
+				new() { UserId = 4, Workload = "loaded"},
+				new() { UserId = 5, Workload = "has few tasks"},
+		};
+
+		public static string sqlQuery =
+			"INSERT INTO \"employees\" (user_id, workload) VALUES" +
+			string.Join(", ", PreparedEmployees.Select(e => $"('{e.UserId}', '{e.Workload}')")) +
+			";";
+	}
+}
