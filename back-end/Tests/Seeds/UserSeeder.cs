6d3bf1b8603c7bc6045eba0eee6ffdd93d482193
Nazg0r <gorchinshiy83@gmail.com>
2024-12-20 16:05:09 +0200

    feat: add UserSeeder

---
+++ back-end/Tests/Seeds/UserSeeder.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Entities;
+
+namespace Tests.Seeds
+{
+	public static class UserSeeder
+	{
+		public static IEnumerable<User> PreparedUsers =>
+		new User[]
+		{
+				new() { Name = "Oleksandr", Surname = "Shevchenko"},
+				new() { Name = "Dmytro", Surname = "Kovalchuk"},
+				new() { Name = "Andriy", Surname = "Grytsenko"},
+				new() { Name = "Volodymyr", Surname = "Tkachenko"},
+				new() { Name = "Kateryna", Surname = "Moroz"},
+				new() { Name = "Olha", Surname = "Sydenko"},
+				new() { Name = "Iryna", Surname = "Petrenko"},
+		};
+
+		public static string sqlQuery =
+			"INSERT INTO \"users\" (name, surname) VALUES" +
+			string.Join(", ", PreparedUsers.Select(p => $"('{p.Name}', '{p.Surname}')")) +
+			";";
+	}
+}
