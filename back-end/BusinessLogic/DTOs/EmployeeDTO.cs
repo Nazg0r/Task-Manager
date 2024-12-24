62170e5ed2c87da912f0e2f07cc0770f5d111890
Nazg0r <gorchinshiy83@gmail.com>
2024-12-24 14:56:19 +0200

    feat: create EmployeeDTO

---
+++ back-end/BusinessLogic/DTOs/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Server
+{
+	public class EmployeeDTO
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Surname { get; set; }
+		public string Workload { get; set; } = "free";
+	}
+}
