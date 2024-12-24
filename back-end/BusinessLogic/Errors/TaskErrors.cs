f0162deefddf768c04303a7692150d23a91b5a75
Nazg0r <gorchinshiy83@gmail.com>
2024-12-24 22:28:24 +0200

    feat: add new error types for task service

---
+++ back-end/BusinessLogic/Errors/TaskErrors.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace BusinessLogic.Errors
+{
+	public static class TaskErrors
+	{
+		public static Error TasksNotFound => 
+			Error.Custom(ErrorTypes.NotFound, "Tasks.NotFound", $"Failed to get tasks.");
+
+		public static Error TaskNotFound =>
+			Error.Custom(ErrorTypes.NotFound, "Task.NotFound", $"Failed to get task.");
+	}
+}
