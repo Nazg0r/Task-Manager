7ac54a792314f851b904fd7eb9959d04e86a1c70
Nazg0r <gorchinshiy83@gmail.com>
2024-12-24 14:54:46 +0200

    feat: create Task and Employee models

---
+++ back-end/BusinessLogic/Models/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic.Models
+{
+	public class EmployeeModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public string Workload { get; set; }
+		public IEnumerable<int>? TaskIds { get; set; }
+	}
+}
---
+++ back-end/BusinessLogic/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BusinessLogic.Models
+{
+	public class TaskModel
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string Priority { get; set; }
+		public string State { get; set; }
+		public string TimeToFinish { get; set; }
+		public string CreationDate { get; set; }
+		public IEnumerable<EmployeeModel> Employees { get; set; }
+	}
+}
